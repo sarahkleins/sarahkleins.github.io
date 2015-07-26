d1958684853954fbd188bf3c2e7db83cb36c34d6
sarahkleins <kleinman.sarah@gmail.com>
2015-07-25 20:15:59 -0400

    initial commit

---
+++ src/scripts/ready.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class Outset {
+  constructor( options ) {
+    document.addEventListener('DOMContentLoaded', this.create(), false);
+  }
+
+  create() {
+
+  }
+}
+
+// INSTANCE
+
+var outset = new Outset({
+
+});
